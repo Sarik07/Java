4da15d4de439c3bfe8b7472d496ce9156860dc0b
Hasan Bin Ekram (Sarik) <83214460+Sarik07@users.noreply.github.com>
2021-11-07 12:16:18 +0600

    Add files via upload

---
+++ JAVA/nsyml/src/nsyml/Nsyml.java
@@ -0,0 +1,247 @@
+
+package nsyml;
+
+import java.util.Scanner;
+
+
+public class Nsyml {
+    public static void main(String[] args) {
+       
+//        System.out.println(" ");
+//       Scanner sc= new Scanner(System.in); 
+//       int max;
+//       String name= sc.nextLine();
+//        System.out.println(name);
+//       System.out.println("Add marks for 1st");
+//       float a= sc.nextInt();
+//       System.out.println("Add marks for 2nd");
+//       float b= sc.nextInt();
+//       System.out.println("Add marks for 3rd");
+//       float c= sc.nextInt();
+//       System.out.println("Add marks for 4th");
+//       float d= sc.nextInt();
+//       System.out.println("Add marks for 5th");
+//       float e= sc.nextInt();
+//       
+//       float sum = a+b+c+d+e;
+//        System.out.print("Total marks:  ");
+//        System.out.println(sum);
+//        double percentage = (sum / 500) * 100 ;
+//        System.out.println(percentage);
+//        boolean b1=sc.hasNextInt();
+//        System.out.println(b1);
+//          String name = "Harry";
+//        System.out.println(name);
+//        int value = name.length();
+        //System.out.println(value);
+
+        //String lstring = name.toLowerCase();
+        //System.out.println(lstring);
+
+        //String ustring = name.toUpperCase();
+        //System.out.println(ustring);
+
+        //String nonTrimmedString = "     Harry     ";
+        //System.out.println(nonTrimmedString);
+
+        //String trimmedString = nonTrimmedString.trim();
+        //System.out.println(trimmedString);
+
+        //System.out.println(name.substring(1));
+        //System.out.println(name.substring(1,5));
+
+        //System.out.println(name.replace('r', 'p'));
+        //System.out.println(name.replace("r", "ier"));
+
+        //System.out.println(name.startsWith("Har"));
+        //System.out.println(name.endsWith("dd"));
+
+        //System.out.println(name.charAt(4));
+
+        //String modifiedName = "Harryrryrry";
+        //System.out.println(modifiedName.indexOf("rry"));
+        //System.out.println(modifiedName.indexOf("rry", 4));
+        //System.out.println(modifiedName.lastIndexOf("rry", 7));
+
+        //System.out.println(name.equals("Harry"));
+//        System.out.println(name.equalsIgnoreCase("HarRY"));
+//
+//        System.out.println("I am escape sequence\tdouble quote");
+//***STRING CHANGE***\\
+//String name = "SaRiK is a good boy";
+//        System.out.println(name.toLowerCase());
+//        System.out.println(name.toUpperCase());
+//        System.out.println(name.replace(" ", "_"));
+//        System.out.println(name.indexOf(" "));
+//***INCOME TAX***\\
+//        float income=sc.nextFloat();
+//        float tax=0;
+//        if (income<=2500){
+//            System.out.println("No tax");
+//        }
+//        else if(income > 2500 && income < 5000){
+//            tax= income*5/100;
+//            System.out.println(tax);
+//        }
+//        else if(income>5000 && income<100){
+//            tax= income*10/100;
+//            System.out.println(tax);
+//        }
+//        
+//        else{
+//            tax= income*30/100;
+//            System.out.println(tax);
+//        }
+        //***factorial***s\\
+//       int fact=1;
+//       int i;
+//       int n=sc.nextInt();
+//       for(i=n;i>=1;i--){
+//           fact=fact*i;
+//       }
+//        System.out.println(fact);
+//       String s= "Hello Sarik";
+//       String p= "Sarik";
+       
+//       for(int i=0;i<=s.length();i++)
+//       {
+//            System.out.println(s.charAt(i));
+//       }
+//       String s="Sarik";
+//       String n="Is good";
+//        System.out.println(s+""+n);
+//       int number[]=new int[10];
+//       int sum=0;
+//       for(int i=0; i<number.length;i++)
+//       {
+//           number[i]=sc.nextInt();
+//           sum=sum+number[i];
+//          
+//       }
+//       System.out.println();
+//       System.out.println("sum : " +sum);
+//        System.out.println("Avg : " +sum/number.length);
+        
+//          int nrt=sc.nextInt();
+//          int ind=sc.nextInt();
+          
+//         int max=number[0];
+//          for(int i=0;i<=number.length;i++)
+//          { 
+//             
+//              if(number[i]>max){
+//                  
+//               max=number[i];
+//              }
+//              
+//             
+//          
+//             
+//          }
+//              System.out.println(max);
+//         int number[]={10,11,12,13,14,15};
+//        int max=number[0];
+//         int i;
+//        System.out.println("Max=" + max);
+//         for (i=1;i<=number.length;i++)
+//         {
+//             if(number[i]>max){
+//                 max=number[i];
+//             System.out.println(max);
+//             }
+//        System.out.println(max);
+//         }
+//    
+//      System.out.println(max);
+    
+//        int number[]={10,11,12,14,20,13};
+//        int max=number[0];
+//    for(int i=1; i<number.length; i++)
+//    {
+////        System.out.println(max);
+//        if(number[i]>max){
+////            System.out.println(max);
+//            max=number[i];
+////            System.out.println(max);
+//        }
+////        System.out.println(max);
+//    }
+//    System.out.println(max);
+// int number[]={10,11,12,14,20,13};
+// 
+// for(int i=number.length-1; i>0; i--){
+//    System.out.println(number[i]);
+// }
+//        int number[]={10,11,12,14,20,13,11};
+//        int i;
+//        for( i=1; i<number.length; i++){
+//            for(int j=i+1;j<number.length;j++){
+//                if(number[i]==number[j]){
+//                  System.out.println(number[i]);
+//                }
+//            }
+//        }
+//        int number[]={10,11,12,14,20,13,11};
+//        int i;
+//        int temp=number[number.length-1];
+//        for(i=number.length-1;i>0;i--)
+//        {
+//            number[i]=number[i-1];
+//        }
+//        number[0]=temp;
+//       for(i=0;i<number.length;i++){
+//           System.out.println(number[i]);
+//       }
+//Scanner sc = new Scanner(System.in);
+//String input = sc.nextLine();
+//char[] me=input.toCharArray();
+//int number=0;
+//int letter=0;
+//int space=0;
+//for (int i=0;i<= input.length();i++)
+//{
+//    if (Character.isDigit(me[i]))
+//    {
+//        number++;
+//    }
+//    else if (Character.isAlphabetic(me[i]))
+//    {
+//        letter++;
+//    }
+//    else if (Character.isSpaceChar(me[i]))
+//    {
+//        space++;
+//    }
+//}
+//        System.out.println("Number : " +number);
+//        System.out.println("Letter : " +letter);
+//        System.out.println("Space : " +space);
+//    Scanner sc=new Scanner(System.in);
+//    String temp=sc.nextLine();
+//    char ch[]=temp.toCharArray();
+//    int number=0;
+//    int space=0;
+//    int letter=0;
+//    int other=0;
+//    for(int i=0;i<temp.length();i++){
+//        if(Character.isDigit(ch[i])){
+//            number++;
+//            }
+//        else if(Character.isSpaceChar(ch[i])){
+//            space++;
+//            }
+//        
+//        else if(Character.isAlphabetic(ch[i])){
+//            letter++;
+//            }
+//        else{
+//                other ++;
+//        }
+//    }
+//        System.out.println("Number : " +number);
+//        System.out.println("Letter : " +letter);
+//        System.out.println("Space : " +space);
+//        System.out.println("Other : " +other);
+        
+    }
+}
